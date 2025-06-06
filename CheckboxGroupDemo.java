0898eea34f5fb6ce6178d24ccb09ba6451774062
Sayan-Dutta-2003 <100032007+Sayan-Dutta-2003@users.noreply.github.com>
2025-06-06 14:57:09 +0530

    Add object-oriented java assignment

--- CheckboxGroupDemo.java
+++ CheckboxGroupDemo.java
@@ -1,43 +1,69 @@
-// 43.	Write a Java program to demonstrate checkbox group.
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CheckboxGroupDemo extends Frame {
+public class CheckboxGroupDemo extends JFrame implements ActionListener {
+
+    JCheckBox option1, option2, option3;
+    JButton submitButton;
+    JLabel resultLabel;
+
     public CheckboxGroupDemo() {
         setTitle("Checkbox Group Demo");
         setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
-        Label label = new Label("Select your gender:");
+        // Create checkboxes
+        option1 = new JCheckBox("Option 1");
+        option2 = new JCheckBox("Option 2");
+        option3 = new JCheckBox("Option 3");
 
-        CheckboxGroup genderGroup = new CheckboxGroup();
-        Checkbox male = new Checkbox("Male", genderGroup, false);
-        Checkbox female = new Checkbox("Female", genderGroup, false);
-        Checkbox other = new Checkbox("Other", genderGroup, false);
+        // Add checkboxes to frame
+        add(option1);
+        add(option2);
+        add(option3);
 
-        Label resultLabel = new Label("Selected: None");
+        // Submit button
+        submitButton = new JButton("Submit");
+        submitButton.addActionListener(this);
+        add(submitButton);
 
-        male.addItemListener(e -> resultLabel.setText("Selected: Male"));
-        female.addItemListener(e -> resultLabel.setText("Selected: Female"));
-        other.addItemListener(e -> resultLabel.setText("Selected: Other"));
-
-        add(label);
-        add(male);
-        add(female);
-        add(other);
+        // Label to display selected checkboxes
+        resultLabel = new JLabel("Selected: None");
         add(resultLabel);
 
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) {
-                dispose();
-            }
-        });
+        setLocationRelativeTo(null); // Center window
+    }
 
-        setVisible(true);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        StringBuilder selectedOptions = new StringBuilder("Selected: ");
+
+        boolean anySelected = false;
+        if (option1.isSelected()) {
+            selectedOptions.append("Option 1 ");
+            anySelected = true;
+        }
+        if (option2.isSelected()) {
+            selectedOptions.append("Option 2 ");
+            anySelected = true;
+        }
+        if (option3.isSelected()) {
+            selectedOptions.append("Option 3 ");
+            anySelected = true;
+        }
+
+        if (!anySelected) {
+            selectedOptions.append("None");
+        }
+
+        resultLabel.setText(selectedOptions.toString());
     }
 
     public static void main(String[] args) {
-        new CheckboxGroupDemo();
+        SwingUtilities.invokeLater(() -> {
+            new CheckboxGroupDemo().setVisible(true);
+        });
     }
 }
